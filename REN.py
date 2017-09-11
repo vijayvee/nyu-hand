3f29cead5baaae03563ae3d6b62dd90e80d7adff
Vijay <vijayvee@gmail.com>
2017-09-11 11:33:24 -0400

    Add trainer

--- REN.py
+++ REN.py
@@ -5,37 +5,20 @@ from dataset import NYUDataset
 from utils import *
 
 class REN:
-    def __init__(self,importer_path,useCache=True,sequence,Nmax,batch_size,input_size=128,cacheDir='/media/data_cifs/lu/cache'):
+    def __init__(self,batch_size,nImgs,input_size=128,l2_lambda=0.001,cacheDir='/media/data_cifs/lu/cache'):
         """
         Implementation of Region Ensemble Network('https://arxiv.org/pdf/1702.02447.pdf') for Hand pose estimation
-        :param importer_path: Path to the input dataset
-        :param useCache: Flag to use cached data from a pickle file
-        :param cacheDir: Directory where the cached pickle file is stored (Only if useCache is set)
         :param batch_size: Batch size of the REN model
         :param sequence: Sequence to load, eg. train/val/test
         :param Nmax: Maximum number of images to load
         :param input_size: Size of the input image to REN
         :param n_joints: Number of joints for the network to predict
+        :param l2_lambda: Lambda for l2 regularization
         """
         self.batch_size = batch_size
-        self.importer = NYUImporter(importer_path,useCache=useCache,cacheDir=cacheDir)
-        self.sequence = sequence
-        self.imgSeq = importer.loadSequence(sequence,Nmax=Nmax,allJoints=True)
         self.n_joints = 36
-        self.dataset = NYUDataset(imgSeqs=[self.imgSeq])
-        self.data, self.labels, seqconfig, train_com3D,train_M = dataset.imgStackDepthOnly(sequence)
         self.batch_shape = [self.batch_size,input_size,input_size,1]
-        self.data = self.data.squeeze(1)
-        self.nImgs = len(self.data)
-
-    def fetch_data_batch(self):
-        """
-        Function to fetch a data batch of depth images and the groung truth (joint locations) in xyz
-        """
-        idxs = np.random.choice(self.nImgs,self.batch_size)
-        curr_batch_dpt = self.data[idxs]
-        curr_batch_gt = self.labels[idxs]
-        return curr_batch_dpt,curr_batch_gt
+        self.l2_lambda = l2_lambda
 
     def init_variables(self):
         """
@@ -44,11 +27,51 @@ class REN:
         self.imgs = tf.placeholder(tf.float32,self.batch_shape,name='input_imgs')
         self.gt_3d = tf.placeholder(tf.float32,[self.batch_size,self.n_joints,3],name='ground_truth_joints')
         self.dropout_prob = tf.placeholder(tf.float32,name='drouput_keep_probability')
+        self.conv_layers,self.fc_layers = [],[]
 
     def build_model(self):
+        """
+        Function to build our REN network
+        """
         x = self.imgs
         x = conv2d(input=x,k_size=3,output_dim=16,name='conv_1')
+        self.conv_layers.append(x)
         x = conv2d(input=x,k_size=3,output_dim=16,name='conv_2')
-        x = pool2d(x)
-        x = conv2d(input=x,k_size=3,output_dim=16,name='conv_')
-        x = conv2d(input=x,k_size=3,output_dim=16,name='conv_1')
+        self.conv_layers.append(x)
+        x = max_pool(input=x,name='max_pool_1')
+        pool1 = x
+        self.conv_layers.append(x)
+        x = conv2d(input=x,k_size=3,output_dim=32,name='conv_3')
+        self.conv_layers.append(x)
+        x = conv2d(input=x,k_size=3,output_dim=32,name='conv_4') + pool1
+        self.conv_layers.append(x)
+        x = max_pool(input=x,name='max_pool2')
+        pool2 = x
+        self.conv_layers.append(x)
+        x = conv2d(input=x,k_size=3,output_dim=64,name='conv_5')
+        self.conv_layers.append(x)
+        x = conv2d(input=x,k_size=3,output_dim=64,name='conv_6') + pool2
+        self.conv_layers.append(x)
+        x = max_pool(input=x,name='max_pool3')
+        self.conv_layers.append(x)
+        top_l,top_r,bot_l,bot_r = x[:,0:6,0:6,:],x[:,0:6,6:,:],x[:,6:,0:6,:],x[:,6:,6:,:]
+        fc1 = Linear(top_l,2048,'fc1',True,self.dropout_prob)
+        fc2 = Linear(top_l,2048,'fc2',True,self.dropout_prob)
+        fc3 = Linear(top_l,2048,'fc3',True,self.dropout_prob)
+        fc4 = Linear(top_l,2048,'fc4',True,self.dropout_prob)
+        self.fc_layers.extend((fc1,fc2,fc3,fc4))
+        fc_final_inp = tf.concat([fc1,fc2,fc3,fc4],axis=1)
+        fc_final = Linear(fc_final_inp,self.numJoints*3,name='output_regressor',True,self.dropout_prob,act='tanh')
+        self.fc_layers.append(fc_final)
+        print "Built model"
+        return self.conv_layers,self.fc_layers
+
+    def get_loss(self,fc_final):
+        """
+        Function to create the loss op for REN.
+        """
+        assert self.labels.get_shape().as_list() == fc_final.get_shape().as_list()
+        regr_loss = tf.reduce_mean(tf.nn.l2_loss(self.labels-fc_final))
+        l2_reg = tf.add_n([tf.nn.l2_loss(var) for var in tf.trainable_variables()])
+        self.loss = self.l2_lambda*l2_reg + (1-self.l2_lambda)*regr_loss
+        return self.loss
--- tf_utils.py
+++ tf_utils.py
@@ -3,9 +3,9 @@ import numpy as np
 
 def weight_bias_variable(input_shape,output_dim,name):
     """
-    Function to return a tf Variable
-    :param shape: Shape of the new variable to create
-    :param name: Name of the new variable to create
+    Function to return tf variables for kernel and bias
+    :param input_shape: Shape of the input for convolution
+    :param name: Name of the conv layer
     """
     n,h,w,c = input_shape
     W = tf.get_variable(dtype=tf.float32,
@@ -18,9 +18,64 @@ def weight_bias_variable(input_shape,output_dim,name):
                     initializer=tf.contrib.layers.xavier_initializer())
     return W,b
 
+def weight_bias_variable_linear(input_shape,output_dim,name):
+    """
+    Function to return a tf variables for weight and bias
+    :param input_shape: Shape of the new variable to create
+    :param name: Name of the new variable to create
+    """
+    a,input_dim = input_shape
+    W = tf.get_variable(dtype=tf.float32,
+                    name=name+'_W',
+                    shape=[input_dim,output_dim],
+                    initializer=tf.contrib.layers.xavier_initializer())
+    b = tf.get_variable(dtype=tf.float32,
+                    name=name+'_b',
+                    shape=output_dim,
+                    initializer=tf.contrib.layers.xavier_initializer())
+    return W,b
+
 def conv2d(input,k_size,output_dim,name):
+    """
+    Function to perform a convolution operation on 'input'.
+    :param input: Input to perform conv on
+    :param k_size: Size of conv kernel
+    :param output_dim: Number of conv kernels to be used
+    :param name: Name of the current conv layer
+    """
     with tf.variable_scope(name):
         W,b = weight_bias_variable(input.get_shape().as_list(),output_dim,name)
         x = tf.nn.conv2d(input,W,strides=[1,1,1,1],padding='SAME')
         out = tf.nn.relu(tf.nn.bias_add(x,b))
+        return out
+
+def max_pool(input,name):
+    """
+    Function to perform a max pooling operation on 'input'.
+    :param input: Input to perform max pooling on
+    :param name: Name of the current max pool layer
+    """
+    with tf.variable_scope(name):
+        out = tf.nn.max_pool(input,ksize=[1,2,2,1],padding='SAME',strides=[1,2,2,1])
+        return out
+
+def Linear(input,output_dim,name,dropout=False,dropout_prob=None,act='relu'):
+    """
+    Function to perform a linear transformation of 'input'.
+    :param input: Input to perform linear transformation on.
+    :param output_dim: Dimension of output
+    :param name: Name of the linear layer.
+    """
+    if len(input.get_shape())!=2:
+        n = input.get_shape()
+        input = tf.reshape(input,[n,-1])
+    with tf.variable_scope(name):
+        W,b = weight_bias_variable_linear(input.get_shape().as_list(),output_dim,name)
+        out = tf.nn.xw_plus_b(input,W,b)
+        if act == 'relu':
+            out = tf.nn.relu(out)
+        else:
+            out = tf.nn.tanh(out)
+        if dropout:
+            out = tf.nn.dropout(out,dropout_prob)
         return out
