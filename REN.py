383d2d2c5ababd1c2a7a4d23588492fc4c4d165a
Vijay <vijayvee@gmail.com>
2017-09-23 10:23:43 -0400

    Add testing script

--- REN.py
+++ REN.py
@@ -32,39 +32,40 @@ class REN:
         """
         Function to build our REN network
         """
-        x = self.imgs
-        x = conv2d(input=x,k_size=3,output_dim=16,name='conv_1')
-        self.conv_layers.append(x)
-        x = conv2d(input=x,k_size=3,output_dim=16,name='conv_2')
-        self.conv_layers.append(x)
-        x = max_pool(input=x,name='max_pool_1')
-        pool1 = conv2d(input=x,k_size=1,output_dim=32,name='residual_1')
-        self.conv_layers.append(x)
-        x = conv2d(input=x,k_size=3,output_dim=32,name='conv_3')
-        self.conv_layers.append(x)
-        x = conv2d(input=x,k_size=3,output_dim=32,name='conv_4') + pool1
-        self.conv_layers.append(x)
-        x = max_pool(input=x,name='max_pool2')
-        pool2 = conv2d(input=x,k_size=1,output_dim=64,name='residual_2')
-        self.conv_layers.append(x)
-        x = conv2d(input=x,k_size=3,output_dim=64,name='conv_5')
-        self.conv_layers.append(x)
-        x = conv2d(input=x,k_size=3,output_dim=64,name='conv_6') + pool2
-        self.conv_layers.append(x)
-        x = max_pool(input=x,name='max_pool3')
-        self.conv_layers.append(x)
-        top_l,top_r,bot_l,bot_r = x[:,0:6,0:6,:],x[:,0:6,6:,:],x[:,6:,0:6,:],x[:,6:,6:,:]
-        print type(top_l),top_l
-        fc1 = Linear(top_l,2048,'fc1',True,self.dropout_prob)
-        fc2 = Linear(top_l,2048,'fc2',True,self.dropout_prob)
-        fc3 = Linear(top_l,2048,'fc3',True,self.dropout_prob)
-        fc4 = Linear(top_l,2048,'fc4',True,self.dropout_prob)
-        self.fc_layers.extend((fc1,fc2,fc3,fc4))
-        fc_final_inp = tf.concat([fc1,fc2,fc3,fc4],axis=1)
-        self.fc_final = Linear(fc_final_inp,self.n_joints*3,'output_regressor',True,self.dropout_prob,act='tanh')
-        self.fc_layers.append(self.fc_final)
-        print "Built model"
-        return self.conv_layers,self.fc_layers
+        with tf.device('/gpu:0'):
+            x = self.imgs
+            x = conv2d(input=x,k_size=3,output_dim=16,name='conv_1')
+            self.conv_layers.append(x)
+            x = conv2d(input=x,k_size=3,output_dim=16,name='conv_2')
+            self.conv_layers.append(x)
+            x = max_pool(input=x,name='max_pool_1')
+            pool1 = conv2d(input=x,k_size=1,output_dim=32,name='residual_1')
+            self.conv_layers.append(x)
+            x = conv2d(input=x,k_size=3,output_dim=32,name='conv_3')
+            self.conv_layers.append(x)
+            x = conv2d(input=x,k_size=3,output_dim=32,name='conv_4') + pool1
+            self.conv_layers.append(x)
+            x = max_pool(input=x,name='max_pool2')
+            pool2 = conv2d(input=x,k_size=1,output_dim=64,name='residual_2')
+            self.conv_layers.append(x)
+            x = conv2d(input=x,k_size=3,output_dim=64,name='conv_5')
+            self.conv_layers.append(x)
+            x = conv2d(input=x,k_size=3,output_dim=64,name='conv_6') + pool2
+            self.conv_layers.append(x)
+            x = max_pool(input=x,name='max_pool3')
+            self.conv_layers.append(x)
+            top_l,top_r,bot_l,bot_r = x[:,0:6,0:6,:],x[:,0:6,6:,:],x[:,6:,0:6,:],x[:,6:,6:,:]
+            print type(top_l),top_l
+            fc1 = Linear(top_l,2048,'fc1',True,self.dropout_prob)
+            fc2 = Linear(top_l,2048,'fc2',True,self.dropout_prob)
+            fc3 = Linear(top_l,2048,'fc3',True,self.dropout_prob)
+            fc4 = Linear(top_l,2048,'fc4',True,self.dropout_prob)
+            self.fc_layers.extend((fc1,fc2,fc3,fc4))
+            fc_final_inp = tf.concat([fc1,fc2,fc3,fc4],axis=1)
+            self.fc_final = Linear(fc_final_inp,self.n_joints*3,'output_regressor',True,self.dropout_prob,act='tanh')
+            self.fc_layers.append(self.fc_final)
+            print "Built model"
+            return self.conv_layers,self.fc_layers
 
     def print_all_layers(self):
         """
--- trainer.py
+++ trainer.py
@@ -8,9 +8,10 @@ from dataset import NYUDataset
 dataset_path = '/media/data_cifs/lu/NYU/dataset/' #Path where dataset splits train, test and val are stored
 cache_path = '/media/data_cifs/lu/cache' #Directory where cache files are stored while reading the dataset
 useCache = True
-Nmax = 100
+Nmax = 50000
 sequence = 'train'
-print_loss_every = 10
+print_loss_every = 100
+save_model_every = 5000
 imp = NYUImporter(dataset_path,useCache=useCache,cacheDir=cache_path)
 trainSeq = imp.loadSequence(sequence,Nmax=Nmax,allJoints=True) #Sequence of training images
 dataset = NYUDataset([trainSeq])
@@ -25,28 +26,42 @@ def fetch_curr_batch(batch_size):
     yield train_data[idxs],train_gt3D[idxs]
 
 
-def run(batch_size,Nmax,l2_lambda,n_epochs,learning_rate):
+def run(batch_size,Nmax,l2_lambda,n_epochs,learning_rate,init_vars=True):
     ren = REN(batch_size,Nmax,l2_lambda=l2_lambda)
     ren.init_variables()
     ren.build_model()
     ren.print_all_layers()
     loss = ren.get_loss(ren.fc_final)
     opt,train_step = ren.get_optimizer()
-    gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.5)
-    sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
-    sess.run(tf.global_variables_initializer())
-    nIter = int(Nmax*n_epochs/batch_size)
-    for i in xrange(nIter):
-        gen = fetch_curr_batch(batch_size)
-        curr_imgs,curr_gt = gen.next()
-        curr_gt = curr_gt.reshape([batch_size,-1])
-        curr_imgs = curr_imgs.transpose(0,2,3,1)
-        curr_loss,_ = sess.run([loss,train_step],feed_dict={ren.imgs:curr_imgs,ren.gt_3d:curr_gt,ren.dropout_prob:1.0})
-        if i%print_loss_every == 0:
-            print "Iter-{} loss: {}".format(i,curr_loss)
+    gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=1.0)
+    saver = tf.train.Saver()
+    with tf.Session() as sess:
+#        sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
+        if init_vars:
+            sess.run(tf.global_variables_initializer())
+            print "Initialized variables"
+        else:
+            saver_ = tf.train.Saver()
+            saver_.restore(sess,tf.train.latest_checkpoint('ckpt/'))
+            print "Restored variables"
+        nIter = int(Nmax*n_epochs/batch_size)
+        for i in xrange(nIter):
+            gen = fetch_curr_batch(batch_size)
+            curr_imgs,curr_gt = gen.next()
+            curr_gt = curr_gt.reshape([batch_size,-1])
+            curr_imgs = curr_imgs.transpose(0,2,3,1)
+            curr_loss,_ = sess.run([loss,train_step],feed_dict={ren.imgs:curr_imgs,ren.gt_3d:curr_gt,ren.dropout_prob:1.0})
+            if i%print_loss_every == 0:
+                print "Iter-{} loss: {}".format(i,curr_loss)
+            if i%save_model_every == 0:
+                print "Saving iter-{} nmax- {} l2- {} epochs- {} lr- {}".format(i,Nmax,l2_lambda,n_epochs,learning_rate)
+                saver.save(sess,'ckpt/NYU_Hand_{}_{}_{}_{}_{}.ckpt'.format(i,Nmax,l2_lambda,n_epochs,learning_rate),global_step=i)
+            if i==nIter-1:
+                print "Training complete..\nSaving iter-{} nmax- {} l2- {} epochs- {} lr- {}".format(i,Nmax,l2_lambda,n_epochs,learning_rate)
+                saver.save(sess,'ckpt/NYU_Hand_{}_{}_{}_{}_Final.ckpt'.format(Nmax,l2_lambda,n_epochs,learning_rate))
 
 def main():
-    run(16,1000,0.0001,100,0.0005)
+    run(32,Nmax,0.001,10,0.005,init_vars=False)
 
 if __name__=="__main__":
     main()
